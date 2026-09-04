4205d9a426fdfa28283f2e2a8f0fb8437a0e1c2d
airita-grinberga <airita@inbox.lv>
2026-09-04 09:18:13 +0300

    Programmas 1.v.

---
+++ src/git1/Kaulins.java
@@ -0,0 +1,12 @@
+package git1;
+
+public class Kaulins {
+
+	public static void main(String[] args) {
+		int skaitlis;
+		skaitlis = (int)(Math.random()*6)+1;
+		System.out.println("Uzkrita skaitlis: " + skaitlis);
+
+	}
+
+}
