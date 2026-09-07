36216c91167227e12479d09eef2a6fe811f10700
airita-grinberga <airita@inbox.lv>
2026-09-07 10:07:37 +0300

    Ieviesta iespēja reižu skaitu ievadīt no konsoles

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,12 +1,21 @@
 package git1;
 
+import java.util.Scanner;
+
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int skaitlis;
-		skaitlis = (int)(Math.random()*6)+1;
-		System.out.println("Uzkrita skaitlis: " + skaitlis);
-
+		int skaitlis, reizes;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Cik reizes mest kauliņu? ");
+		reizes = scan.nextInt();
+		
+		for (int i=0; i<reizes; i++) {
+			skaitlis = (int)(Math.random()*6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+		}
+		scan.close();
 	}
 
 }
