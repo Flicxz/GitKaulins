527b7d6bf7823740a3e28f8edad5105ba4fa22e3
airita-grinberga <airita@inbox.lv>
2026-09-07 10:01:26 +0300

    Pievienots cikls

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,14 +1,16 @@
 package git1;
 
 public class Kaulins {
-	static void MestKaulinu() {
+	static void MestKaulinu(int reizes) {
 		int skaitlis;
-		skaitlis = (int)(Math.random()*6)+1;
-		System.out.println("Uzkrita skaitlis: " + skaitlis);
+		for (int i=0; i<reizes; i++) {
+			skaitlis = (int)(Math.random()*6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+		}
 	}
 	
 	public static void main(String[] args) {
-		MestKaulinu();
+		MestKaulinu(5);
 	}
 
 }
