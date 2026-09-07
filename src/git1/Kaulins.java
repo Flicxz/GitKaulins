520e6a5167872d72a32dd52e48001ae1131504d0
airita-grinberga <airita@inbox.lv>
2026-09-07 09:55:51 +0300

    Ieviesta metode

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,12 +1,14 @@
 package git1;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	static void MestKaulinu() {
 		int skaitlis;
 		skaitlis = (int)(Math.random()*6)+1;
 		System.out.println("Uzkrita skaitlis: " + skaitlis);
-
+	}
+	
+	public static void main(String[] args) {
+		MestKaulinu();
 	}
 
 }
