940af48c83dd87c43874799e4bfaf3ed814780d0
Flicxz <nezjanis7@gmail.com>
2026-09-16 14:36:48 +0300

    Uztaisīta lielāka cipara izdrukāšana

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Kaulins {
 	static int MestKaulinu(int reizes) {
-		int skaitlis, min=6;
+		int skaitlis, min=6, max=0;
 		for (int i=0; i<reizes; i++) {
 			skaitlis = (int)(Math.random()*6)+1;
 			System.out.println("Uzkrita skaitlis: " + skaitlis);
+			if(skaitlis > max) {
+				max = skaitlis;
+			}
 			if (skaitlis < min) min = skaitlis;
 		}
+		System.out.println("Lielākais skaitlis no visiem metieniem: " + max);
 		return min;
 	}
 
