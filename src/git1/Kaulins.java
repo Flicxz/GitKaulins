5baf9e07a687e4399bc2d2254658ba763db13b83
Flicxz <nezjanis7@gmail.com>
2026-09-16 15:01:31 +0300

    Kauliņa matieni saglabāti masīvā

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class Kaulins {
 	static int MestKaulinu(int reizes) {
-		int skaitlis, min=6, max=0;
+		int min=6, max=0;
+		int skaitlis[] = new int [reizes];
 		for (int i=0; i<reizes; i++) {
-			skaitlis = (int)(Math.random()*6)+1;
-			System.out.println("Uzkrita skaitlis: " + skaitlis);
-			if(skaitlis > max) {
-				max = skaitlis;
+			skaitlis[i] = (int)(Math.random()*6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis[i]);
+			if(skaitlis[i] > max) {
+				max = skaitlis[i];
 			}
-			if (skaitlis < min) min = skaitlis;
+			if (skaitlis[i] < min) min = skaitlis[i];
+		}
+		for (int i=0; i<skaitlis.length; i++) {
+			System.out.println(skaitlis[i]);
 		}
 		System.out.println("Lielākais skaitlis no visiem metieniem: " + max);
 		return min;
